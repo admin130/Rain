f2ac44130221f36d318d8a56df838803ddc83490
Juan Garcia <juan@apps502.com>
2019-11-04 20:56:00 -0600

    Repo init.

---
+++ src/dev/jag/rain/Game.java
@@ -0,0 +1,31 @@
+package dev.jag.rain;
+
+public class Game implements Runnable{
+	
+	public static int width = 300;
+	public static int height = width * 9 / 16;
+	public static int scale = 3;
+	
+	private Thread thread;
+	
+	public synchronized void start() {
+		thread = new Thread(this,"Display");
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		try {
+			thread.join();
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
