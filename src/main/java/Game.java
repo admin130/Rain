de07743937b0d2d41c2a273f202dd4dd3087fbb5
Juan Garcia <juan@luna.com.gt>
2019-11-06 21:41:35 -0600

    ADD: FPS counter in title and in console.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -15,6 +15,7 @@ public class Game extends Canvas implements Runnable{
     public static int width = 300;
     public static int height = width * 9 / 16;
     public static int scale = 3;
+    public static String title = "Rain";
 
     private Thread thread;
     private boolean running = false;
@@ -51,19 +52,31 @@ public class Game extends Canvas implements Runnable{
     public void run() {
 
         long lastTime = System.nanoTime();
+        long secondsTimer = System.currentTimeMillis();
         final double nanoSeconds = 1000000000.0 / 60.0;
         double delta = 0;
-
+        int frames = 0;
+        int updates = 0;
         while(running) {
             long now = System.nanoTime();
             delta += (now-lastTime) / nanoSeconds;
 
             while (delta>=1){
                 update();
+                updates++;
                 delta--;
             }
 
             render();
+            frames++;
+
+            if(System.currentTimeMillis() - secondsTimer > 1000){
+                secondsTimer += 1000;
+                frame.setTitle(title + " | " + updates + " ups, " + frames + " fps");
+                System.out.println(updates + " ups, " + frames + " fps");
+                updates = 0;
+                frames = 0;
+            }
         }
         stop();
     }
@@ -96,7 +109,7 @@ public class Game extends Canvas implements Runnable{
     public static void main(String[] args) {
         Game game = new Game();
         game.frame.setResizable(false);
-        game.frame.setTitle("Rain");
+        game.frame.setTitle(Game.title);
         game.frame.add(game);
         game.frame.pack();
         game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
